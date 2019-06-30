ca66e092b1cd7c9307cfd791f0618d47ce1c0612
github username <github email address>
2019-06-30 12:48:51 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  patch 'posts/:id', to: 'posts#update'
+  get 'posts/:id/:edit', to: 'posts#edit'
+
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
